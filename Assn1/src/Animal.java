149fd6e733436627d9486bb7f65d24330257a510
Jake Landowski <c0d3.mag1@gmail.com>
2017-04-06 12:07:52 -0700

    Finished javadocs

--- Assn1/src/Animal.java
+++ Assn1/src/Animal.java
@@ -1,58 +1,107 @@
-import java.util.Random
+import java.util.Random;
 
+/**
+ * This class instantiates a basic Animal object with a name, x/y coordinate values,
+ * and a max speed. It is used in the AnimalMain class.
+ *
+ * @author                  Jacob Landowski
+ * @version                 %I% %G%
+ * @since                    1.0
+ */
 public class Animal
 {
     private final Random rand = new Random();
-    private final int[] DOMAIN = { -20, 20 };
-    private final int[] RANGE = { -20, 20 };
+    private final int[] DOMAIN = { -20, 20 };     //  SET TO [-2 <=> 2] FOR EASIER TESTING
+    private final int[] RANGE = { -20, 20 };      //  FIGHTS ARE RARE AT [-20 <=> 20] RANGE
     private final int[] MAX_SPD_RANGE = { 2, 5 };
     private String name;
-    private int x, y;       //  -20 <--> 20
-    private double maxspd; //   2 <--> 5
+    private int x, y;       //  -20 <=> 20
+    private int maxspd; //   2 <=> 5
+    private boolean isfighting;
     
+    /**
+     * Default constructor which calls its parameterized constructor
+     * Passes String "unknown", int 0, int 0, int 4.
+     *
+     */
     public Animal()
     {
-        name = "unknown";
-        x = y = 0;
-        maxspd = 4;
+        this("unknown", 0, 0, 4);
     }
     
-    public Animal(int initX, int initY, String initName, int initMaxSpd)
+    /**
+     * Parameterized constructor.
+     *
+     * @param initName      the name of the animal.
+     * @param initX         the x coordinate, will be capped at DOMAIN values.
+     * @param initY         the y coordinate, will be capped at RANGE values
+     * @param initMaxSpd    the max movement value used when determining how much to randomly move,
+     *                      starting from 1 to this number, will be capped at
+     *                      <code>MAX_SPD_RANGE</code> values.
+     * @see                 #DOMAIN
+     * @see                 #RANGE
+     * @see                 #MAX_SPD_RANGE
+     * @see                 #setX(int)
+     * @see                 #setY(int)
+     * @see                 #setMaxSpd(int)
+     */
+    public Animal(String initName, int initX, int initY, int initMaxSpd)
     {
         name = initName;
         setX(initX);
         setY(initY);
-        maxspd = setMaxSpd(initMaxSpd);
+        setMaxSpd(initMaxSpd);
+        isfighting = false;
     }
     
+    /**
+     * Moves this animal in a random direction a distance calculated from
+     * random 1 to maxspd.
+     *
+     * @see                 #maxspd
+     */
     public void move()
     {
-        int dir = rand.nextInt(1, 5);
-        int dist = rand.nextInt(1, maxspd+1);
+        int dir = rand.nextInt(4) + 1;
+        int dist = rand.nextInt(maxspd) + 1;
         
-        switch(dir)
+        switch(dir) //  MOVE EACH DIRECTION
         {
             case 1:
-                setX(-dist);
+                setX(x - dist);
                 break;
             case 2:
-                setX(dist);
+                setX(x + dist);
                 break;
             case 3:
-                setY(-dist);
+                setY(y - dist);
                 break;
             case 4:
-                setY(dist);
+                setY(y + dist);
                 break;
         }
     }
     
+    /**
+     * Tests to see if this Animal and the passed Animal objects have
+     * x and y that are both equal to each other, returns true/false.
+     *
+     * @param other         The seperate Animal object instance to test against.
+     * @return              <code>True</code> if this.x == other.x and this.y == other.y;
+     *                      <code>false</code> otherwise.
+     */
     public boolean touching(Animal other)
     {
         if(x == other.getX() && y == other.getY()) return true;
         else return false;
     }
     
+    /**
+     * Set this Animal's x. Caps out at DOMAIN values.
+     *
+     * @param num           The number to set x to.
+     * @see                 #DOMAIN
+     */
     private void setX(int num)
     {
         if(num >= DOMAIN[0] && num <= DOMAIN[1]) x = num;
@@ -60,6 +109,12 @@ public class Animal
         else if(num > DOMAIN[1])                 x = DOMAIN[1];
     }
     
+    /**
+     * Set this Animal's y. Caps out at RANGE values.
+     *
+     * @param num           The number to set y to.
+     * @see                 #RANGE
+     */
     private void setY(int num)
     {
         if(num >= RANGE[0] && num <= RANGE[1]) y = num;
@@ -67,15 +122,49 @@ public class Animal
         else if(num > RANGE[1])                y = RANGE[1];
     }
     
+    /**
+     * Set this Animal's maxspd. Caps out at MAX_SPD_RANGE values.
+     *
+     * @param num           The number to set maxspd to.
+     * @see                 #MAX_SPD_RANGE
+     */
     private void setMaxSpd(int num)
     {
         if(num >= MAX_SPD_RANGE[0] &&
            num <= MAX_SPD_RANGE[1])     maxspd = num;
         else if(num < MAX_SPD_RANGE[0]) maxspd = MAX_SPD_RANGE[0];
-        else if(num > MAX_SPD_RANGE[1]) maxpsd = MAX_SPD_RANGE[1];
+        else if(num > MAX_SPD_RANGE[1]) maxspd = MAX_SPD_RANGE[1];
     }
     
-    public String toString() { return name + " [" + x + ", " + y + "]" ; }
-    public int getX()        { return x; }
-    public int getY()        { return y; }
+    /**
+     * Set this animal's fighting status : true or false.
+     *
+     * @param status        The new fighting status passed <code>true</code>
+     *                      or <code>false</code>.
+     * @see                 #isfighting
+     */
+    public void setFighting(boolean status) { isfighting = status; }
+    
+    /**
+     * Returns Animal's name and [x, y] coordinates as a string.
+     *
+     * @return              name [x, y]
+     */
+    public String toString()    { return name + " [" + x + ", " + y + "]"; }
+    /**
+     * @return              name
+     */
+    public String getName()     { return name; }
+    /**
+     * @return              x
+     */
+    public int getX()           { return x; }
+    /**
+     * @return              y
+     */
+    public int getY()           { return y; }
+    /**
+     * @return              isfighting
+     */
+    public boolean isFighting() { return isfighting; }
 }
--- Assn1/src/AnimalMain.java
+++ Assn1/src/AnimalMain.java
@@ -3,27 +3,261 @@
 
 import java.util.Random;
 
+/**
+ * <p>This class utilizes the Animal class and creates an Array of them.
+ * The amount is determined by <code>NUM_ANIMALS</code>. The animal names
+ * are pulled from the <code>ANIMAL_NAMES</code> array and the amount of names
+ * must match the number of animals in <code>NUM_ANIMALS</code>.</p>
+ *
+ * <p>Main prints each Animal to the console, and then starts a loop
+ * which runs a set amount of times specified by <code>SCENARIO_LENGTH</code>.
+ * This loop moves each animal, then checks their locations to see who are
+ * touching and will print their fight to the console. It finishes by printing
+ * the animals again to show their final locations</p>
+ *
+ * @author                  Jacob Landowski
+ * @version                 %I% %G%
+ * @since                   1.0
+ */
 public class AnimalMain
 {
-    private final int NUM_ANIMALS = 5;
-    private final int ANIMAL_NAMES = { "Zebra", "Giraffe", "Gazelle", "Lion", "Hyena" };
+    private static final int NUM_ANIMALS = 5;
+    private static final String[] ANIMAL_NAMES = { "Zebra", "Giraffe", "Gazelle", "Lion", "Hyena" };
+    private static final int SCENARIO_LENGTH = 25;
     
+    /**
+     * <p>Main method.</p>
+     * <ul>
+     * <li>Creates an Animal array <code>animals</code></li>
+     * <li>Creates an Random object <code>rand</code></li>
+     * <li>Calls <code>populate()</code></li>
+     * <li>Calls <code>listAnimals()</code></li>
+     * <li>Calls <code>startScenario()</code></li>
+     * <li>Calls <code>printAnimals()</code></li>
+     * </ul>
+     *
+     * @param args          String array of options when starting program
+     * @see                 Animal
+     * @see                 #populate(Animal[], Random)
+     * @see                 #listAnimals(Animal[])
+     * @see                 #startScenario(Animal[])
+     * @see                 #printAnimals(Animal[])
+     */
     public static void main(String args[])
     {
         Animal[] animals = new Animal[NUM_ANIMALS];
         Random rand = new Random();
+        
         populate(animals, rand);
+        listAnimals(animals);
+        startScenario(animals);
+        System.out.println("The final locations are...\n");
+        printAnimals(animals);
     }
     
-    private void populate(Animal[] arr, Random rand)
+    /**
+     * Populate the animals array with new animals with random
+     * locations (-20 <=> 20) and speeds (2 <=> 5).
+     *
+     * @param arr           the empty Animal array
+     * @param rand          the Random object
+     * @see                 Animal
+     * @see                 Random
+     */
+    private static void populate(Animal[] arr, Random rand)
     {
-        for(int i = 0; i < arr; i++)
+        for(int i = 0; i < arr.length; i++)
         {
-            int x = rand.nextInt(-20, 21);
-            int y = rand.nextInt(-20, 21);
-            int spd = rand.nextInt(2, 6);
+            int x = rand.nextInt(41) - 20;
+            int y = rand.nextInt(41) - 20;
+            int spd = rand.nextInt(4) + 2;
             
             arr[i] = new Animal(ANIMAL_NAMES[i], x, y, spd);
+        }
+    }
+    
+    /**
+     * Loop through the Animal array and print the animals.
+     *
+     * @param animals       the populated Animal array
+     * @see                 {@link Animal}
+     */
+    private static void listAnimals(Animal[] animals)
+    {
+        for(int i = 0; i < animals.length; i++)
+        {
+            System.out.println("A new animal joins the field: " + animals[i]);
+            pause(150);
+        }
+    }
+    
+    /**
+     * Start the movement/fight loop. Moves animals then checks for fights.
+     * printing a line of asterisks per iteration.
+     *
+     * @param animals       the populated Animal array
+     * @see                 {@link Animal}
+     * @see                 {@link #moveAnimals()}
+     * @see                 {@link #checkFights()}
+     */
+    private static void startScenario(Animal[] animals)
+    {
+        String line = "*********************";
+        
+        System.out.println(line);
+        
+        for(int i = 0; i < SCENARIO_LENGTH; i++)
+        {
+            moveAnimals(animals);
+            checkFights(animals);
+            System.out.println(line);
+            pause(150);
+        }
+    }
+    
+    /**
+     * Loop through the Animal array and call the move() method
+     * on each one. Prints movement feedback after moving all animals.
+     *
+     * @param animals       the populated Animal array
+     * @see                 {@link Animal#move()}
+     */
+    private static void moveAnimals(Animal[] animals)
+    {
+        for(int i = 0; i < animals.length; i++) animals[i].move();
+        System.out.println("Animals moving...\n");
+        //printAnimals(animals); FOR TESTING
+    }
+    
+    /**
+     * Loops through the Animal array, selecting each Animal
+     * and then comparing that against each Animal in the
+     * array. Does not check if the current Animal <code>isfighting</code>
+     * The loop will not check if the indexes are the same so it does not
+     * compare the same Animal against itself. It then calls the <code>touching()</code>
+     * method on each comparison to check if they are in the same position.
+     * If true, they are both set to <code>isfighting = true</code> and their
+     * index is saved in the <code>indexes</code> array to mark them as already
+     * being in a fight, to prevent being chosen again in the following loop checks.
+     * If a <code>fightHappened</code> then the marked Animals have their names
+     * appended to the <code>fighting</code> array and <code>printFight()</code>
+     * is called.
+     *
+     * @param animals       the populated Animal array
+     * @see                 {@link Animal#isfighting}
+     * @see                 {@link Animal#touching()}
+     * @see                 {@link Animal#getName()}
+     * @see                 {@link Animal#setFighting()}
+     * @see                 {@link #printFight()}
+     */
+    private static void checkFights(Animal[] animals)
+    {
+            //  HOLDS MAX NUM OF ENCOUNTERS POSSIBLE
+        String[] fighting = new String[NUM_ANIMALS];
+        
+        for(int i = 0; i < animals.length; i++)
+        {
+            Animal ani = animals[i];
+            
+            if( !ani.isFighting() )
+            {
+                int numFighting = 0;
+                
+                    //  KEEPS TRACK OF INDEXES OF ANIMALS PUT IN FIGHTS
+                int[] indexes = new int[NUM_ANIMALS];
+                boolean fightHappened = false;
+                
+                for(int j = 0; j < animals.length; j++)
+                {
+                    Animal other = animals[j];
+                    
+                    if( (i != j) && !other.isFighting() &&
+                        ani.touching(other) )   //  IF NOT ITSELF, AND OTHER NOT IN
+                    {                           //  A FIGHT, AND BOTH TOUCHING
+                        indexes[numFighting] = j;
+                        numFighting++;              //  TRACK OTHER
+                        ani.setFighting(true);      //  BOTH SET TO FIGHTING
+                        other.setFighting(true);
+                        fightHappened = true;
+                    }
+                }
+                    
+                if(fightHappened)
+                {
+                    indexes[numFighting] = i;   //  ALSO TRACK THIS ANIMAL
+                    numFighting++;
+                    
+                    for(int j = 0; j < numFighting; j++)
+                    {                   //  GET THE NAME OF EACH ANIMAL BEING TRACKED
+                        fighting[j] = animals[indexes[j]].getName();
+                    }
+                    
+                    printFight(fighting, numFighting);
+                }
+            }
+        }
+                //  RESET ANIMALS TO NOT FIGHTING
+        setAnimalsNotFighting(animals);
+    }
+    
+    /**
+     * Prints "FIGHT : " along with the animal names passed in the array
+     *
+     * @param fighting      The array of names from each Animal involved
+     *                      in a single fight
+     * @param numFighting   The number of animals in the fight, used for
+     *                      looping the correct amount of times
+     * @see                 {@link #checkFights()}
+     */
+    private static void printFight(String[] fighting, int numFighting)
+    {
+        System.out.print("FIGHT : " + fighting[0]);
+        
+        for(int i = 1; i < numFighting; i++)
+        {
+            System.out.print(" vs " + fighting[i]);
+        }
+            
+        System.out.println("\n");
+    }
+    
+    /**
+     * Loop through each Animal and mark them as no longer fighting
+     *
+     * @param animals       The populated Animal array
+     * @see                 {@link Animal#isfighting}
+     * @see                 {@link Animal#setFighting()}
+     */
+    private static void setAnimalsNotFighting(Animal[] animals)
+    {
+        for(int i = 0; i < animals.length; i++) animals[i].setFighting(false);
+    }
+    
+    /**
+     * Loop through each Animal and print them.
+     *
+     * @param animals       The populated Animal array
+     */
+    private static void printAnimals(Animal[] animals)
+    {
+        for(int i = 0; i < animals.length; i++)
+        {
+            System.out.println(animals[i]);
+        }
+    }
+    
+    /**
+     * Cause the program/thread to sleep for n milliseconds.
+     *
+     * @param ms        The time in milliseconds to pause the program
+     */
+    private static void pause(int ms)
+    {
+        try { Thread.sleep(ms); }
+        
+        catch(InterruptedException err)
+        {
+            Thread.currentThread().interrupt();
         }
     }
 }
